3145c9741c01a00292ede50d2c74d25b5c50195c
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-20 16:00:04 -0300

    iniciar busca da api

--- src/script/main.js
+++ src/script/main.js
@@ -1,2 +1,6 @@
 let livros = []
-const endpointDaAPI = 
+const endpointDaAPI = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyC1A94acXjEpgBvl8rsh3cKexV3VxOiXyc"
+
+let teste = document.getElementById("teste")
+
+console.log(endpointDaAPI)
