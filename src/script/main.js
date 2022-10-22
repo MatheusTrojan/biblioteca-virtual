977704928771469fcd4f1aa9614af5b81eaba11d
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-22 11:08:31 -0300

    mostra resultado na tela

--- src/script/main.js
+++ src/script/main.js
@@ -1,28 +1,46 @@
-
+let livros = []
 
 let buscarLivro = document.getElementById("btn-pesquisar")
 buscarLivro.addEventListener("click", pesquisaLivro)
 
 async function pesquisaLivro() {
     const nomeLivro = document.getElementById("nome-livro")
-    const linkDaAPI = await fetch (`https://www.googleapis.com/books/v1/volumes?q=${nomeLivro.value}`)  
-    const resultadoBusca = await linkDaAPI.json();
     if (nomeLivro.value == "") {
         alert("Por favor preencha o campo de busca")
     } else {
-        console.log(resultadoBusca)
+        let titulo = "";
+        let autor = "";
+        let imagem = "";
+        let url = "";
+
+        const linkDaAPI = await fetch (`https://www.googleapis.com/books/v1/volumes?q=${nomeLivro.value}`)  
+        const resultadoBusca = await linkDaAPI.json();
+        console.log(resultadoBusca.items)
+        
+        for (i = 0; i < resultadoBusca.items.length; i++) {
+            let livro = "";
+            titulo = resultadoBusca.items[i].volumeInfo.title
+            autor = resultadoBusca.items[i].volumeInfo.authors
+            imagem = resultadoBusca.items[i].volumeInfo.imageLinks.thumbnail
+            url = resultadoBusca.items[i].volumeInfo.infoLink
+
+            livro += "<div class='livro__infos'><h3 class='livro__titulo'>" + titulo + "</h3>"
+            livro += "<h4 class='livro__autor'>" + autor + "</h4>"
+            livro += "<img class='livro__imagem' src=" + imagem + ">"
+            livro += "<a href='" + url + "'><button class='livro__saiba-mais'>Saiba Mais</button></a></div>"
+
+            livros.push(livro)
+            console.log(imagem)
+
+        }
+        nomeLivro.value = ""
+        mostraLivroNaTela(livros)
     }
-    
-
-
 }
 
+function mostraLivroNaTela(livros) {
+    let elementoListaLivros = document.getElementById("resultadoFinal")
 
-// getBuscarLivrosDaAPI()
+    elementoListaLivros.innerHTML = livros 
+}
 
-// async function getBuscarLivrosDaAPI() {
-//     const res = await fetch(linkDaAPI)
-//     livros = await res.json()
-
-//     console.log(livros)
-// }
