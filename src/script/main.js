bbe088f0706240ab42cb28f883e4378a6a569186
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-22 10:16:40 -0300

    consegue buscar o nome do livro

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,28 @@
-let livros = []
-const endpointDaAPI = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyC1A94acXjEpgBvl8rsh3cKexV3VxOiXyc"
 
-let teste = document.getElementById("teste")
 
-console.log(endpointDaAPI)
+let buscarLivro = document.getElementById("btn-pesquisar")
+buscarLivro.addEventListener("click", pesquisaLivro)
+
+async function pesquisaLivro() {
+    const nomeLivro = document.getElementById("nome-livro")
+    const linkDaAPI = await fetch (`https://www.googleapis.com/books/v1/volumes?q=${nomeLivro.value}`)  
+    const resultadoBusca = await linkDaAPI.json();
+    if (nomeLivro.value == "") {
+        alert("Por favor preencha o campo de busca")
+    } else {
+        console.log(resultadoBusca)
+    }
+    
+
+
+}
+
+
+// getBuscarLivrosDaAPI()
+
+// async function getBuscarLivrosDaAPI() {
+//     const res = await fetch(linkDaAPI)
+//     livros = await res.json()
+
+//     console.log(livros)
+// }
